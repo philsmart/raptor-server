eb49fd1d7aa10de3ad496c108ffa57c7f53df5dd
philsmart <philsmart@v3059-0a9004c5.wifi.cf.ac.uk>
2017-01-17 16:01:51 +0000

    first commit

---
+++ src/main/java/uk/ac/cardiff/raptor/server/RaptorServerApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.cardiff.raptor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaptorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaptorServerApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/ac/cardiff/raptor/server/RaptorServerApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.cardiff.raptor.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaptorServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
